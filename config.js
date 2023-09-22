f904dd916964f6d9b9b84d53451d849136579c02
ideepakpatel <143080180+ideepakpatel@users.noreply.github.com>
2023-09-22 12:59:39 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,71 @@
+
+export const Exchangelink = [
+    "https://wazirx.com/invite/sp7pvbt6?utm_source=finstreet&utm_medium=affiliate&utm_campaign=regnow-btn", 
+    "https://bit.ly/2BJxccc",
+   
+]
+
+export const currencies = {
+    "INR": {
+        "name": "Rupees",
+        "symbol": "₹"
+    }
+    
+};
+
+export const assets = {
+    "BTC": {
+        "name": "Bitcoin",
+        "symbol": "BTC"
+    },
+    "ETH": {
+        "name": "Etherium",
+        "symbol": "ETH"
+    },
+    "USDT": {
+        "name": "Tether",
+        "symbol": "USDT"
+    }
+    ,
+    "XRP": {
+        "name": "Ripple",
+        "symbol": "XRP"
+    }
+    ,
+    "TRX": {
+        "name": "Tron",
+        "symbol": "TRX"
+    }
+    ,
+    "DASH": {
+        "name": "DASH",
+        "symbol": "DASH"
+    }
+    ,
+    "ZEC": {
+        "name": "Zcash",
+        "symbol": "ZEC"
+    }
+    ,
+    "XEM": {
+        "name": "NEM",
+        "symbol": "XEM"
+    }
+    ,
+    "IOST": {
+        "name": "IOST",
+        "symbol": "IOST"
+    },
+    "WIN": {
+        "name": "WINK",
+        "symbol": "WIN"
+    },
+    "BTT": {
+        "name": "BTT",
+        "symbol": "BTT"
+    },
+     "WRX": {
+        "name": "WRX",
+        "symbol": "WRX"
+    }
+};
